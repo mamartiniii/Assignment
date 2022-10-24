9dc72d84728007b28675f07e4b6464c0b5c39da6
Francesco Sala <francesco.sala@epfl.ch>
2022-10-24 14:42:55 +0200

    created code to evaluate order of convergence

---
+++ plot_error.m
@@ -0,0 +1,53 @@
+clear
+close all
+clc
+
+% The following code calls the function nozzle_1d to solve the problem of
+% assignment 1, changing the number of nodes (N, hence changing the
+% dimension of each CV), and the under-relaxation coefficients.
+
+
+m = 1;
+N = 21;
+alpha_p = 0.01;
+alpha_u = 0.01;
+toll_u = 1e-6;
+toll_p = toll_u;
+it_max = 1000;
+
+L=2;       %[m]
+rho=1;     %[kg/m^3]
+A_in=0.5;  %[m^2]
+A_out=0.1; %[m^2]
+p_0=10;    %[Pa]
+p_out=0;   %[Pa]  NB: pressure is relative
+A=@(x) A_in+(A_out- A_in)/L.*x;
+
+NN = [10,20,40, 80, 160];
+hh = 1./NN;
+errvect_u = zeros(size(hh));
+errvect_p = zeros(size(hh));
+
+
+
+for i =1:length(NN)
+    N = NN(i);
+    [u, p, x_u, x_p] = nozzle_1d(A, N, alpha_p, alpha_u, toll_u, toll_p, it_max, m);
+    u_exact= A_out./A(x_u)*sqrt(2*p_0/rho);
+    p_exact= p_0.*(1-(A_out./A(x_p)).^2);
+    errvect_u(i) = max(abs(u_exact -u));
+    errvect_p(i) = max(abs(p_exact - p));
+end
+
+% the plot shows that the error decreases with 1/N, as expected
+
+loglog(NN, errvect_p, "-o", NN, errvect_u, '-o', NN, 1./NN, "--", NN, 1./(NN.^2), "--", "LineWidth",2)
+title("Empirical estimation of order of accuracy of the SIMPLE algorithm", "interpreter", "latex")
+xlabel("Number of nodes \(N\)", "Interpreter","latex")
+ylabel("Error", "interpreter", "latex")
+grid on
+legend("\(err_p\)", "\(err_u\)", "\(1/N\)", "\(1/N^2\)",  "interpreter", "latex");
+
+fprintf("The SIMPLE method is a 1st order method (due to the QUICK algorithm, which uses the upwind approximation)\n")
+
+
